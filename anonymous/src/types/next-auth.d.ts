8dd625f4a86bcea61f1fdfbf760db14cc6b993cc
saurabh <saurabhsharmauk03@gmail.com>
2026-09-24 02:15:44 +0530

    here we modify the interface for session and JWt

--- anonymous/src/types/next-auth.d.ts
+++ anonymous/src/types/next-auth.d.ts
@@ -1,6 +1,7 @@
 // here we creating additional/modify existing data types
 //again modify /declare 'interface user' which directly solve the problem faced in the option.ts file where we used/declare callback function
 import 'next-auth'
+import { DefaultSession } from 'next-auth';
 
 declare module 'next-auth'{
 interface User{
@@ -9,4 +10,29 @@ isVerified?: boolean;
 isAcceptingMessages?: boolean;
 username?: string
 }
+
+//again modify for session
+interface Session{
+  user: {
+    _id?: string;
+isVerified?: boolean;
+isAcceptingMessages?: boolean;
+username?: string
 }
+& DefaultSession['user']
+  }
+}
+
+//here we modify interface for jwt
+declare module 'next-auth/jwt'{
+  interface JWT {
+    _id?: string;
+    isVerified?: boolean;
+    isAcceptingMessage?: boolean;
+    username?: string
+  }
+}
+
+
+
+
